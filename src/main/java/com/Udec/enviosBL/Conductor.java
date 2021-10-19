2940596873a5889dddd4823e29af2f5ecae8a0b8
RaulilloSierra <raulitosierradiaz@gmail.com>
2021-10-19 12:08:28 -0500

    Cambios iniciales

---
+++ src/main/java/com/Udec/enviosBL/Conductor.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Udec.enviosBL;
+
+/**
+ *
+ * @author SALA DE INTERNET
+ */
+public class Conductor {
+    String ID, Nombre, Telefono;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+    
+    
+}
