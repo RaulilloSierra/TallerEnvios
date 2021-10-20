424f9f6dd164823377de82de512c15c0336f7355
RaulilloSierra <raulitosierradiaz@gmail.com>
2021-10-19 20:52:36 -0500

    Cambios 19.10.2021 8:52 pm

--- src/main/java/com/Udec/enviosDAL/Conexion.java
+++ src/main/java/com/Udec/enviosDAL/Conexion.java
@@ -1,10 +1,24 @@
 
 package com.Udec.enviosDAL;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author SALA DE INTERNET
  */
 public class Conexion {
+    String strConexion = "";
+    Connection conn = null;
     
+    public Conexion(){
+        try {
+            Class.forName("");
+            conn = DriverManager.getConnection(strConexion);
+            System.out.println("Conexion establecida");
+        } catch (Exception e) {
+            System.out.println("Imposible conexion:"+e);
+        }
+    }
 }
